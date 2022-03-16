6b34f0a9a14d7be7fd509bbe96dfa24e4b8bc748
JoseLuisOB <96042700+JoseLuisOB@users.noreply.github.com>
2022-03-16 16:50:35 -0600

    Update MLX90614-ESP32CAM-MQTT-I2C.ino

--- ESP32CAM/MLX90614-ESP32CAM-MQTT-I2C/MLX90614-ESP32CAM-MQTT-I2C.ino
+++ ESP32CAM/MLX90614-ESP32CAM-MQTT-I2C/MLX90614-ESP32CAM-MQTT-I2C.ino
@@ -1,4 +1,4 @@
-/*
+/*Temperatura
  * Programa completo de IoT con sensor de temperatura Infrarrojo
  * por: Hugo Escalpelo
  * Fecha: 04 febrero de 2022
@@ -21,12 +21,12 @@
 #include <Wire.h>
 
 //Datos de WiFi
-const char* ssid = "AXTEL XTREMO-18D6";  // Aquí debes poner el nombre de tu red
-const char* password = "038C18D6";  // Aquí debes poner la contraseña de tu red
+const char* ssid = "OviRab";  // Aquí debes poner el nombre de tu red
+const char* password = "99121976";  // Aquí debes poner la contraseña de tu red
 
 //Datos del broker MQTT
-const char* mqtt_server = "192.168.15.29"; // Si estas en una red local, coloca la IP asignada, en caso contrario, coloca la IP publica
-IPAddress server(192,168,15,29);
+const char* mqtt_server = "192.168.1.108"; // Si estas en una red local, coloca la IP asignada, en caso contrario, coloca la IP publica
+IPAddress server(192,168,1,108);
 
 // Objetos
 WiFiClient espClient; // Este objeto maneja los datos de conexion WiFi
